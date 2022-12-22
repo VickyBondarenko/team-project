6c983822a15267f8a7fd620dad1c483e44af17bc
VickyBondarenko <107431952+VickyBondarenko@users.noreply.github.com>
2022-12-22 17:30:24 +0000

    Deploying to gh-pages from @ VickyBondarenko/team-project@a64b922f5e032dd3d719dda5db42bf3a1a4c4e0d 🚀

---
+++ index.d9ee741c.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-product-modal-open]"),closeModalBtn:document.querySelector("[data-product-modal-close]"),modal:document.querySelector("[data-product-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.d9ee741c.js.map
