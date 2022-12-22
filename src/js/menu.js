c030d88340fa1f8d923bd52845c885c703e65613
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2022-12-22 10:19:33 +0200

    save before shutdown

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,29 +1,5 @@
-// const refs = {
-//   menuBtn: document.querySelector('.menu-btn'),
-//   menuBtnOpen: document.getElementById('close-icon'),
-//   menuBtnClose: document.getElementById('open-icon'),
-//   menuDiv: document.querySelector('.menu')
-// }
-// console.log(refs.menuBtnOpen);
-// refs.menuBtn.addEventListener('click', () =>{
-//   if (refs.menuDiv.dataset.menuVisible == 0){
-//     refs.menuBtn.dataset.menuVisible=1;
-//     refs.menuBtnOpen.className.animVal = 'menu-btn'
-//     refs.menuBtnClose.className.animVal = 'menu-btn visually-hidden'
-//     refs.menuDiv.className = 'menu'
-//     console.log('open');
-//   }else{
-//     refs.menuBtn.dataset.menuVisible=0;
-//     refs.menuBtnOpen.className.animVal = 'menu-btn visually-hidden'
-//     refs.menuBtnClose.className.animVal = 'menu-btn'
-//     refs.menuDiv.className = 'menu closed'
-//     console.log('close');
-//   }
-//   console.log(refs.menuBtnClose.className);
-// })
-
 let closed = 'menu is-hidden', opened = 'menu'
-let btnIcon = document.querySelectorAll('.menu-btn__icon-container')
+let btnIcon = document.querySelectorAll('.menu-btn__icon-wrap')
 let mobileMenu = document.querySelector('.menu');
 
 let openBtn = document.querySelectorAll('.menu-btn')
@@ -35,13 +11,13 @@ function openCloseMenu() {
     mobileMenu.style = 'height: 100vh'
     mobileMenu.className = opened;
     mobileMenu.dataset.menuClosed = 0;
-    btnIcon[0].className = 'menu-btn__icon visually-hidden'
-    btnIcon[1].className = 'menu-btn__icon'
+    btnIcon[0].className = 'menu-btn__icon-wrap visually-hidden'
+    btnIcon[1].className = 'menu-btn__icon-wrap'
   } else {
     mobileMenu.className = closed;
     mobileMenu.dataset.menuClosed = 1;
-    btnIcon[1].className = 'menu-btn__icon visually-hidden'
-    btnIcon[0].className = 'menu-btn__icon'
+    btnIcon[1].className = 'menu-btn__icon-wrap visually-hidden'
+    btnIcon[0].className = 'menu-btn__icon-wrap'
     setTimeout(() => {mobileMenu.style = 'height: 1px'},255)
   }
 }
